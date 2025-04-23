a8345fc3aa028217a944021ac6ef1d885caf990c
Bara Al-Sedih <98843912+BaraSedih11@users.noreply.github.com>
2025-04-23 18:06:31 +0300

    Test / add frontend service and magazine to catalog

--- order/order.py
+++ order/order.py
@@ -54,8 +54,7 @@ orders_schema = OrderSchema(many=True)
 
 @app.route('/order/purchase/<int:book_id>', methods=['POST'])
 def purchase(book_id):
-    print(book_id)
-    response = requests.post(f'http://127.0.0.1:5000/catalog/books/{book_id}/decrement')
+    response = requests.post(f'http://127.0.0.1:5000/catalog/books/{book_id}/decreament')
     if response.status_code == 200:
 
         order_date = datetime.now()
